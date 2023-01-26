eeceeb0638804f218de008840de733b3e2c4f8df
Emma Stout <emma.stout01@gmail.com>
2023-01-26 15:00:03 -0600

    feat: update title, formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,18 @@ import { useContext } from 'react';
 import { ThemeContext } from '../ThemeContext';
 import Button from './Button';
 
-const Card = ({item }) => {
+const Card = ({ item }) => {
     const theme = useContext(ThemeContext)
 
-    return <div className={`card card-${theme}`}>
-        <h2>{item.name}</h2>
-        <img src={item.image} alt={item.name} width="150" height="150"/>
-        <div className='item-secondary-text'>{item.description}</div>
-        <div className='item-secondary-text'>${item.price}</div>
-        <Button>Buy item</Button>
-    </div>
+    return (
+        <div className={`card card-${theme}`}>
+            <h2>{item.name}</h2>
+            <img src={item.image} alt={item.name} width="150" height="150" />
+            <div className='item-secondary-text'>{item.description}</div>
+            <div className='item-secondary-text'>${item.price}</div>
+            <Button>Buy item</Button>
+        </div>
+    )
 }
 
 export default Card;
