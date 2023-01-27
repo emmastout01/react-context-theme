206e3196e73e68bc415203a34e9aee302baaf4c0
Emma Stout <emma.stout01@gmail.com>
2023-01-26 21:01:57 -0600

    formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import Button from './Button';
 
 const Footer = ({ theme }) => {
-    return <div className='footer'>
-        <hr />
-        <Button theme={theme}>Subscribe</Button>
-        <Button theme={theme}>Email us</Button>
-    </div>
+    return (
+        <div className='footer'>
+            <hr />
+            <Button theme={theme}>Subscribe</Button>
+            <Button theme={theme}>Email us</Button>
+        </div>
+    )
 }
 
 export default Footer;
